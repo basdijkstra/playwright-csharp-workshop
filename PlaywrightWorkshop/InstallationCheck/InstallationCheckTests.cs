a4bf4698b4d9eedafb179252f720cc93c11c05e2
Bas Dijkstra <bas@ontestautomation.com>
2025-11-05 10:38:10 +0100

    Initial commit

---
+++ PlaywrightWorkshop/InstallationCheck/InstallationCheckTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightWorkshop.InstallationCheck
+{
+    [TestFixture]
+    public class InstallationCheckTests : PageTest
+    {
+        [Test]
+        public async Task CheckWhetherEverythingWorks()
+        {
+            await Page.GotoAsync("https://www.saucedemo.com");
+
+            await Expect(Page).ToHaveTitleAsync("Swag Labs");
+        }
+    }
+}
