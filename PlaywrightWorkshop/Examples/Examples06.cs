7d0ce14526d0a725db1e442e5d71d5136ee0d966
Bas Dijkstra <bas@ontestautomation.com>
2025-11-09 08:36:35 +0100

    Add example, exercise and answer for initializing database via API call

---
+++ PlaywrightWorkshop/Examples/Examples06.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightWorkshop.Examples
+{
+    [TestFixture]
+    public class Examples06 : PageTest
+    {
+        private IAPIRequestContext apiContext;
+
+        [SetUp]
+        public async Task SetUpApiContext()
+        {
+            var headers = new Dictionary<string, string>()
+            {
+                { "Accept", "application/json" },
+            };
+
+            this.apiContext = await this.Playwright.APIRequest.NewContextAsync(new()
+            {
+                BaseURL = "https://jsonplaceholder.typicode.com",
+                ExtraHTTPHeaders = headers
+            });
+        }
+
+        [Test]
+        public async Task UserDataCanBeRetrieved()
+        {
+            var response = await this.apiContext.GetAsync("/users/1");
+            
+            // Playwright-native assertion
+            await Expect(response).ToBeOKAsync();
+
+            // Vanilla NUnit assertion
+            Assert.That(response.Status, Is.EqualTo(200));
+
+        }
+
+        [TearDown]
+        public async Task CleanUp()
+        {
+            await this.apiContext.DisposeAsync();
+        }
+    }
+}
---
+++ PlaywrightWorkshop/Exercises/Exercises06.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using PlaywrightWorkshop.Answers.Pages.ParaBank;
+
+namespace PlaywrightWorkshop.Exercises
+{
+    [TestFixture]
+    public class Exercises06 : PageTest
+    {
+        private IAPIRequestContext apiContext;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            Page.SetDefaultTimeout(5000);
+
+            /**
+             * TODO: Initialize the apiContext object
+             * Use 'https://parabank.parasoft.com' for the BaseURL
+             * Add an Accept header with the value 'application/json'
+             */
+
+        }
+
+        [TestCase("10000", "1000", "12345", "Denied", TestName = "Loan application for 10000 with 1000 down payment is denied")]
+        [TestCase("1000", "500", "12345", "Approved", TestName = "Loan application for 1000 with 500 down payment is approved")]
+        public async Task SubmitLoanApplication_CheckResult_ShouldMatchExpected
+            (string amount, string downPayment, string fromAccountId, string expectedResult)
+        {
+            /**
+             * TODO: Replace the first four statements with an HTTP POST call to /parabank/services/bank/initializeDB
+             * Check that the response status code is equal to 204
+             */
+
+            await Page.GotoAsync("https://parabank.parasoft.com");
+
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Admin Page" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Button, new() { Name = "INIT" }).ClickAsync();
+            await Expect(Page.GetByText("Database Initialized")).ToBeVisibleAsync();
+
+            var loginPage = new LoginPage(Page);
+            await loginPage.Open();
+            await loginPage.LoginAs("john", "demo");
+
+            await new AccountsOverviewPage(Page).SelectMenuItem("Request Loan");
+                        
+            var requestLoanPage = new RequestLoanPage(Page);
+            await requestLoanPage.SubmitLoanRequestFor(amount, downPayment, fromAccountId);
+
+            await Expect(requestLoanPage.TextfieldLoanApplicationResult).ToHaveTextAsync(expectedResult);
+        }
+
+        /**
+         * TODO: Add a [TearDown] method that properly disposes the IAPIRequestContext object
+         */
+
+        [TearDown]
+        public async Task CleanUp()
+        {
+            await this.apiContext.DisposeAsync();
+        }
+    }
+}
