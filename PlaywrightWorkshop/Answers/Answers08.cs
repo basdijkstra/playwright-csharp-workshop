d8a07701ef40d0e8e6d5d166cb6b07eff2f8fa97
Bas Dijkstra <bas@ontestautomation.com>
2025-11-11 11:18:18 +0100

    Add explanation to answers

--- PlaywrightWorkshop/Answers/Answers08.cs
+++ PlaywrightWorkshop/Answers/Answers08.cs
@@ -15,12 +15,6 @@ namespace PlaywrightWorkshop.Answers
         {
             Page.SetDefaultTimeout(5000);
 
-            /**
-             * TODO: Initialize the apiContext object
-             * Use 'https://parabank.parasoft.com' for the BaseURL
-             * Add an Accept header with the value 'application/json'
-             */
-
             var headers = new Dictionary<string, string>()
             {
                 { "Accept", "application/json" },
@@ -31,13 +25,25 @@ namespace PlaywrightWorkshop.Answers
                 BaseURL = "https://parabank.parasoft.com",
                 ExtraHTTPHeaders = headers
             });
+
+            var response = await this.apiContext.PostAsync("/parabank/services/bank/initializeDB");
+            Assert.That(response.Status, Is.EqualTo(204));
         }
 
         [Test]
         public async Task SubmitLoanApplication_CheckResult_ShouldMatchExpected()
         {
-            var response = await this.apiContext.PostAsync("/parabank/services/bank/initializeDB");
-            Assert.That(response.Status, Is.EqualTo(204));
+            /** 
+             * TODO: define a mock response for the API call that the browser makes to
+             *   '* / ** / requestLoan?customerId=12212&amount=10000&downPayment=1000&fromAccountId=12345'
+             *   (remove the spaces).
+             *   
+             * The mock JSON response should have these fields and values:
+             *   responseDate = 1758177294806
+             *   loanProviderName = "My mocked API response"
+             *   approved = true
+             *   accountId = 14121
+             */
 
             await Page.RouteAsync("*/**/requestLoan?customerId=12212&amount=10000&downPayment=1000&fromAccountId=12345", async route =>
             {
@@ -65,13 +71,16 @@ namespace PlaywrightWorkshop.Answers
             var requestLoanPage = new RequestLoanPage(Page);
             await requestLoanPage.SubmitLoanRequestFor("10000", "1000", "12345");
 
+            /**
+             * TODO: add Expect verifications that check that the loan application result is "Approved"
+             *   and that the loan provider name is "My mocked API response". Both text fields are 
+             *   defined in and exposed by the RequestLoanPage object already, so you don't need to
+             *   make changes to that object yourself
+             */
+
             await Expect(requestLoanPage.TextfieldLoanApplicationResult).ToHaveTextAsync("Approved");
             await Expect(requestLoanPage.TextfieldLoanProvider).ToHaveTextAsync("My mocked API response");
         }
-
-        /**
-         * TODO: Add a [TearDown] method that properly disposes the IAPIRequestContext object
-         */
 
         [TearDown]
         public async Task CleanUp()
