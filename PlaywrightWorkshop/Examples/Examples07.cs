f023f37fd63d96581ed4f8dc4ceb44dec59be07e
Bas Dijkstra <bas@ontestautomation.com>
2025-11-11 07:51:36 +0100

    Add examples for API testing

---
+++ PlaywrightWorkshop/Examples/Examples07.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using PlaywrightWorkshop.Examples.Models;
+
+namespace PlaywrightWorkshop.Examples
+{
+    [TestFixture]
+    public class Examples07 : PageTest
+    {
+        private IAPIRequestContext apiContext;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var headers = new Dictionary<string, string>()
+            {
+                { "Accept", "application/json" },
+            };
+
+            this.apiContext = await this.Playwright.APIRequest.NewContextAsync(new()
+            {
+                BaseURL = "https://jsonplaceholder.typicode.com",
+                ExtraHTTPHeaders = headers
+            });
+        }
+
+        [Test]
+        public async Task GetUserData_CheckName_ShouldBeLeanneGraham()
+        {
+            var getUserOneResponse = await apiContext.GetAsync("/users/1");
+
+            Assert.That(getUserOneResponse.Status, Is.EqualTo(200));
+
+            // Using deserialization into a JsonElement
+            var bodyAsJsonElement = await getUserOneResponse.JsonAsync();
+            Assert.That(bodyAsJsonElement.Value.GetProperty("name").GetString(), Is.EqualTo("Leanne Graham"));
+
+            // Using deserialization into a User object
+            var bodyAsTypedObject = await getUserOneResponse.JsonAsync<User>();
+            Assert.That(bodyAsTypedObject.Name, Is.EqualTo("Leanne Graham"));
+        }
+
+        [Test]
+        public async Task PostNewPost_CheckStatusCode_ShouldBeHttpCreated()
+        {
+            var post = new Dictionary<string, object>()
+            {
+                { "title", "my new blog post" },
+                { "body", "awesome content here" },
+                { "userId", 1 }
+            };
+
+            var postPostResponse = await this.apiContext.PostAsync("/posts", new()
+            {
+                DataObject = post
+            });
+
+            Assert.That(postPostResponse.Status, Is.EqualTo(201));
+        }
+
+        [TearDown]
+        public async Task CleanUp()
+        {
+            await this.apiContext.DisposeAsync();
+        }
+    }
+}
---
+++ PlaywrightWorkshop/Examples/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PlaywrightWorkshop.Examples.Models
+{
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
