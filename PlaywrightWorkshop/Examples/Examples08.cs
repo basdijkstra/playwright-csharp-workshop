e66e8158cc9b529b8e01a8914dba4ad53b001e43
Bas Dijkstra <bas@ontestautomation.com>
2025-11-11 12:38:11 +0100

    Add example for API mocking

---
+++ PlaywrightWorkshop/Examples/Examples08.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightWorkshop.Examples
+{
+    [TestFixture]
+    public class Examples08 : PageTest
+    {
+        [SetUp]
+        public async Task SetUp()
+        {
+            Page.SetDefaultTimeout(5000);
+        }
+
+        [Test]
+        public async Task GetFruits()
+        {
+            await Page.RouteAsync("*/**/api/v1/fruits", async route =>
+            {
+                var mockResponse = new[]
+                {
+                    new
+                    {
+                        name = "Some weird kind of fruit",
+                        id = 21
+                    }
+                };
+                
+                await route.FulfillAsync(new()
+                {
+                    Json = mockResponse
+                });
+
+            });
+
+            // Go to the page
+            await Page.GotoAsync("https://demo.playwright.dev/api-mocking");
+
+            // Assert that the Strawberry fruit is visible
+            await Expect(Page.GetByText("Some weird kind of fruit")).ToBeVisibleAsync();
+        }
+    }
+}
